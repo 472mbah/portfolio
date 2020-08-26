3bc7c1816319c8ae9327f8289539d41e0411bc24
Momodou Bah <472mbah@gmail.com>
2020-08-26 19:13:59 +0100

    Updates

---
+++ precache-manifest.af864635f3f28b5e1c75643c4e0b92b7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6897496d0c581eff4c4d5da6ba95c02",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "446543beb65d00a86a8b",
+    "url": "/portfolio/static/css/main.f92ab982.chunk.css"
+  },
+  {
+    "revision": "8e167c314ffc97d9ebaa",
+    "url": "/portfolio/static/js/2.885de9d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.885de9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446543beb65d00a86a8b",
+    "url": "/portfolio/static/js/main.71cd21a8.chunk.js"
+  },
+  {
+    "revision": "8d5151d6cdd6ccebf727",
+    "url": "/portfolio/static/js/runtime-main.7dabe899.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/portfolio/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/portfolio/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/portfolio/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "2467699cb25ec7334d0551ced0fc1e53",
+    "url": "/portfolio/static/media/github.2467699c.png"
+  },
+  {
+    "revision": "e938def8f0924cb41c6c30baa37cab7f",
+    "url": "/portfolio/static/media/link.e938def8.svg"
+  },
+  {
+    "revision": "6d95de4c987f762ac4f7663922bfdab1",
+    "url": "/portfolio/static/media/linked_in.6d95de4c.png"
+  },
+  {
+    "revision": "350bdaa115191b08ab4c26fcc57791d6",
+    "url": "/portfolio/static/media/location.350bdaa1.png"
+  },
+  {
+    "revision": "9e132aec8e572af480d07d0a080120ef",
+    "url": "/portfolio/static/media/time.9e132aec.svg"
+  }
+]);
